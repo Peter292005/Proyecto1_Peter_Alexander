a6c7988f5ef23cbc6eef4e36ec5dec31a6202727
Peter292005 <168543357+Peter292005@users.noreply.github.com>
2024-10-20 22:20:30 -0400

    Estructura de datos completas e hicimos algunas pruebas

---
+++ Proyecto1_Peter_Alexander./src/EstructurasDatos/Vertice.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EstructurasDatos;
+
+import MainClass.Parada;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mateusnaddaf
+ */
+public class Vertice {
+    private Parada parada;
+    private int numVertice;
+    private ListaSimple listaAdyacencia;
+
+    public Vertice(Parada parada) {
+        this.parada = parada;
+        this.numVertice = -1;
+        this.listaAdyacencia = new ListaSimple();
+    }
+
+    public Parada getParada() {
+        return parada;
+    }
+
+    public void setParada(Parada parada) {
+        this.parada = parada;
+    }
+
+    public int getNumVertice() {
+        return numVertice;
+    }
+
+    public void setNumVertice(int numVertice) {
+        this.numVertice = numVertice;
+    }
+
+    public ListaSimple getListaAdyacencia() {
+        return listaAdyacencia;
+    }
+
+    public void setListaAdyacencia(ListaSimple listaAdyacencia) {
+        this.listaAdyacencia = listaAdyacencia;
+    }
+    
+    public void añadirAdyacencia(Vertice vertice){
+        if(!this.listaAdyacencia.buscar(vertice)){
+            this.listaAdyacencia.insertarFinal(vertice);
+        }
+    }
+    
+    public String mostrarListaAdyacencia(){
+        if(!this.listaAdyacencia.esVacia()){
+            return listaAdyacencia.transformar();
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        if(this.mostrarListaAdyacencia() != null){
+            return numVertice + ", " + parada.getNombre() + " -> " + this.mostrarListaAdyacencia();
+        } 
+        return numVertice + ", " + parada.getNombre();
+    }
+    
+    public void mostrar(){
+        JOptionPane.showMessageDialog(null, this.toString());
+    }
+    
+}
+
+
--- Proyecto1_Peter_Alexander./src/proyecto1_peter_alexander/Main.java
+++ Proyecto1_Peter_Alexander./src/proyecto1_peter_alexander/Main.java
@@ -3,6 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package proyecto1_peter_alexander;
+import EstructurasDatos.Cola;
+import EstructurasDatos.Grafo;
+import EstructurasDatos.ListaSimple;
+import EstructurasDatos.Vertice;
+import MainClass.Parada;
 
 /**
  *
@@ -14,7 +19,73 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+//        ListaSimple lista = new ListaSimple();
+//        lista.insertarFinal(1);
+//        lista.insertarFinal(2);
+//        lista.insertarFinal(3);
+//        lista.insertarFinal(4);
+//        
+//        lista.mostrar();
+//        
+//        lista.eliminarFinal();
+//        
+//        lista.mostrar();
+//        
+//        System.out.println(lista.buscar(100));
+//
+//        Cola cola = new Cola();
+//        
+//        cola.mostrar();
+//        
+//        cola.enColar(1);
+//        cola.enColar(2);
+//        cola.enColar(3);
+//        cola.enColar(4);
+//        
+//        cola.mostrar();
+//        
+//        cola.desenColar();
+//        
+//        cola.mostrar();
+        Parada parada = new Parada("California");
+        Parada parada2 = new Parada("Los Cortijos");
+        Parada parada3 = new Parada("Los Dos Caminos");
+        
+        Vertice vertice1 = new Vertice(parada);
+        Vertice vertice2 = new Vertice(parada2);
+        Vertice vertice3 = new Vertice(parada3);
+        
+        vertice1.getListaAdyacencia().insertarFinal(vertice2);
+        
+        vertice2.getListaAdyacencia().insertarFinal(vertice1);
+        vertice2.getListaAdyacencia().insertarFinal(vertice3);
+        
+        vertice3.getListaAdyacencia().insertarFinal(vertice2);
+        
+        System.out.println(vertice1.mostrarListaAdyacencia());
+        System.out.println(vertice2.mostrarListaAdyacencia());
+        
+        vertice1.mostrar();
+        
+        vertice2.mostrar();
+        
+        Grafo grafo = new Grafo();
+        
+        grafo.insertarVertice(vertice1);
+        grafo.insertarVertice(vertice2);
+        grafo.insertarVertice(vertice3);
+        
+        grafo.mostrar();
+        
+         
+         grafo.conectarVertices(vertice1, vertice2);
+         grafo.conectarVertices(vertice2, vertice3);
+         
+         grafo.mostrar();
     }
     
 }
+
+    
+    
+
